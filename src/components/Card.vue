cd0fb964bc26dacb97f70641c74be65b8a8a4edc
mehdi asgari <it1@abharh.ir>
2023-09-19 13:52:42 +0330

    card component

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="card  bg-gradient d-inline-flex p-2 m-3  bg-warning" style="width: 18rem;">
+        <img :src="word.img" alt="">
+        <div class="card-body">
+            <h5 class="card-title">
+                <h2>{{ word.name }}</h2>
+            </h5>
+            <p class="card-text">{{ word.persian }}</p>
+
+            <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                aria-controls="offcanvasRight">منوی باز شو از سمت چپ
+                <i class="bi-tools text-white"></i>
+            </button>
+        </div>
+    </div>
+</template>
+<script setup>
+import { defineProps } from "vue"
+const { word } = defineProps(['word'])
+</script>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,28 +1,11 @@
 <script setup>
-
+import Card from "../components/Card.vue"
+import q from "../assets/dic.json"
+import { ref } from "vue"
+const words = ref(q)
+//console.log(words)
 </script>
 
 <template>
-<div class="card  bg-gradient d-inline-flex p-2 m-3" style="width: 18rem; background-color: #509dd4;">
-   <div class="card-body">
-    <h5 class="card-title">Card title
-      <i class="bi-tools text-white"></i>
-    </h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-<div class="card  bg-gradient d-inline-flex p-2 bg-warning" style="width: 18rem;">
-   <div class="card-body">
-    <h5 class="card-title">Card title
-      <i class="bi-alarm"></i>
-    </h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-
-
+  <Card v-for="word in words" :key="word.id" :word="word" />
 </template>
