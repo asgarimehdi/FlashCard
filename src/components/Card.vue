cebe23e67fe3f99d39c2e9a8fbee3623928e925e
asgari.mehdi <asgarimehdi@outlook.com>
2023-09-25 20:42:09 +0330

    bugs fixed

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,46 +3,49 @@ import { ref } from "vue"
 const showPersian = ref(false)
 const hover = ref(false)
 const { word } = defineProps(['word'])
-const emit = defineEmits(['okRecord','notOkRecord'])
+const emit = defineEmits(['okRecord', 'notOkRecord'])
 const emitOkRecord = (id) => {
     emit("okRecord", id)
 }
 
-const emitNotOkRecord = (id) => {    
+const emitNotOkRecord = (id) => {
     emit('notOkRecord', id)
 }
 
 </script>
 <template>
-    <div class="card  bg-gradient  p-1 m-3  col-xl-3 col-lg-5 col-md-11 col-sm-11 rounded-pill" @mouseover="hover = true"
-        @mouseleave="hover = false" :class="{ 'shadow ': hover, 'shadow-lg': !hover }">
+    <div class="card  bg-gradient  p-1 m-3  col-xl-3 col-lg-5 col-md-11 col-sm-11 rounded-pill" @mouseover="hover = true" 
+        @mouseleave="hover = false" :class="{ 'shadow ': hover, 'shadow-lg': !hover }" @click="showPersian = !showPersian">
         <!-- <img :src="word.img" alt=""> -->
 
 
         <div class="card-body text-center">
-            
-            <h5 class="card-title" @click="showPersian = !showPersian">
+
+            <h5 class="card-title">
                 <span class="title1">{{ word.name }}
-                    
-                <i class="bi bi-translate text-success title2"></i>
-            </span>
-                
+
+
+                </span>
+
             </h5>
-            <p class="card-text vazir " v-if="showPersian">
+            <p class="card-text vazir " v-show="showPersian">
                 <span class="vazir title2">{{ word.persian }}</span>
             </p>
 
         </div>
-        <div class=" text-center" v-if="showPersian">
+        <div class=" text-center" v-show="showPersian">
             <a href="#" @click="emitOkRecord(word.id)" class="card-link btn shadow rounded border-success title2">بلد بودم
                 <i class="bi bi-check-circle-fill text-success"></i>
             </a>
-            <a href="#" @click="showPersian = !showPersian,emitNotOkRecord(word.id)" class="card-link btn shadow rounded border-danger title2">
+            <button  @click="emitNotOkRecord(word.id)"
+                class="card-link btn shadow rounded border-danger title2">
                 بلد نبودم
                 <i class="bi bi-x-square text-danger"></i>
-            </a>
+            </button>
         </div>
-        <div>&nbsp;</div>
+
+
+        <div>&nbsp</div>
     </div>
 </template>
 
--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,41 +4,46 @@ import CardsHeader from "../components/CardsHeader.vue"
 import Result from "../components/Result.vue"
 import q from "../assets/dic.json"
 import { ref } from "vue"
+const num=1
 let words = ref(q)
 const wordsFull = ref(q)
 const level = ref(0)
 const done = []
+
 const onRecordOk = (id) => {
 
   words.value = words.value.filter(i => i.id != id)
   //console.log(words)
-  if (words.value.length < 1) {
+  if (level.value == 2) {
+    done.push(id);
+
+  }
+  if (words.value.length < num) {
     words.value = wordsFull.value
     level.value++
   }
 
-  if (level.value > 2) {
-    console.log('Done')
-    words.value = 0
-  }
-  if (level.value > 1) {
-    done.push(id);
-  }
+
 }
 const onRecordNotOk = (id) => {
 
   if (level.value > 1) { // وقتی در لول آخر هستیم
     words.value = words.value.filter(i => i.id != id)
-  }
-}
+    if (words.value.length < num) {
 
+      level.value++
+    }
+  }
+
+}
+//console.log(done)
 </script>
 
 <template>
   <div class="container-fluid">
     <CardsHeader :barPercentage="level" />
     <div class="row  justify-content-center">
-      <Card v-if="level < 3" v-for="word in words" :key="word.id" :word="word" @notOkRecord="onRecordNotOk"
+      <Card v-if="level < 3" v-for="word in words" :key="word.name" :word="word" @notOkRecord="onRecordNotOk"
         @OkRecord="onRecordOk" />
       <Result v-else :done="done" />
     </div>
