0e3ab0c1579fafb130da448358129060443cf758
mehdi asgari <it1@abharh.ir>
2023-09-26 14:03:43 +0330

    completing result

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,18 +4,19 @@ import CardsHeader from "../components/CardsHeader.vue"
 import Result from "../components/Result.vue"
 import q from "../assets/dic.json"
 import { ref } from "vue"
-const num=1
+const num=14
 let words = ref(q)
 const wordsFull = ref(q)
 const level = ref(0)
 const done = []
+const notDone = []
 
 const onRecordOk = (id) => {
 
   words.value = words.value.filter(i => i.id != id)
   //console.log(words)
   if (level.value == 2) {
-    done.push(id);
+    done.push(findWord(id));
 
   }
   if (words.value.length < num) {
@@ -29,12 +30,16 @@ const onRecordNotOk = (id) => {
 
   if (level.value > 1) { // وقتی در لول آخر هستیم
     words.value = words.value.filter(i => i.id != id)
+    notDone.push(findWord(id));
     if (words.value.length < num) {
 
       level.value++
     }
   }
 
+}
+const findWord=(idd)=>{
+  return wordsFull.value.find(({id})=>id===idd)
 }
 //console.log(done)
 </script>
@@ -45,7 +50,7 @@ const onRecordNotOk = (id) => {
     <div class="row  justify-content-center">
       <Card v-if="level < 3" v-for="word in words" :key="word.name" :word="word" @notOkRecord="onRecordNotOk"
         @OkRecord="onRecordOk" />
-      <Result v-else :done="done" />
+      <Result v-else :done="done" :notDone="notDone" />
     </div>
   </div>
 </template>
--- src/components/Result.vue
+++ src/components/Result.vue
@@ -1,7 +1,25 @@
 <script setup>
-const { done } = defineProps(['done'])
-console.log(done)
+const { done,notDone } = defineProps(['done','notDone'])
+console.log(notDone)
 </script>
 <template>
-    result 
+    <div class="container">
+        <div>
+            <h1> جلسه با موفقیت به پایان رسید</h1>
+            <h3> برای ذخیره در سرور روی گزینه ثبت نهایی کلیک کنید</h3>
+        </div>
+        <ul>
+            <li v-for="word in done" :key="word">
+            <span class="title1">{{ word.name }} </span> 
+           <span class="title2"> {{ word.persian }} </span>
+            </li>
+        </ul>
+        بلد نبودید: 
+        <ul>
+            <li v-for="word in notDone" :key="word">
+            <span class="title1">{{ word.name }} </span> 
+           <span class="title2"> {{ word.persian }} </span>
+            </li>
+        </ul>
+    </div>
 </template>
